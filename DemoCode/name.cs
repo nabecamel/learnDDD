89d2c08e11b02cd82663564c8a51a95959b1478a
nabecamel <wataru_418@icloud.com>
2024-10-18 09:23:57 +0900

    learning ValueObject

---
+++ DemoCode/name.cs
@@ -0,0 +1,13 @@
+using	System;
+
+namespace	DemoCode
+{
+	public	class	Name
+	{
+		public static void	PrintFullName()
+		{
+			var	fullName = "wataru watanabe";
+			Console.WriteLine(fullName);
+		}
+	}
+}
---
+++ main.cs
@@ -0,0 +1,11 @@
+using	System;
+using	DemoCode;
+
+class	Program
+{
+	static	void	Main(string[] args)
+	{
+		Name.PrintFullName();
+	}
+}
+
